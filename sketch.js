065f2740470c1cddd3bbb583d02b1abf1856abd0
Ojaswa Rajput <69306443+OjaswaRajput@users.noreply.github.com>
2021-09-26 13:49:37 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,7 +14,6 @@ function setup() {
 
 	engine = Engine.create();
 	world = engine.world;
-	dustbinObj=new dustbin(1200,650);
 	paperObject=new paper(200,450,40);
 	groundObject=new ground(width/2,670,width,20);
 	//Create a Ground
